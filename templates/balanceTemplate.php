76eccbc45ca5ba91e0b719368dadf92866ab9b45
Oski <oskar.bialek.programista@gmail.com>
2018-03-18 11:11:03 +0100

    added func to show detailed incomes in all periods

--- templates/balanceTemplate.php
+++ templates/balanceTemplate.php
@@ -152,7 +152,7 @@ if(isset($_GET['period'])){
 									$query = "SELECT * FROM Incomes i INNER JOIN incomes_category cat ON i.user_id = cat.user_id 
 											  AND i.category_id = cat.id WHERE i.user_id = '$user_id' ORDER BY amount DESC";
 
-										if(!$result = $manager->dbo->query($query)){
+									if(!$result = $manager->dbo->query($query)){
 									    //echo 'Wystąpił błąd: nieprawidłowe zapytanie...';
 									    return SERVER_ERROR;
 										}
@@ -193,6 +193,82 @@ if(isset($_GET['period'])){
 								//echo 'Błąd: ' . $e->getMessage();
 								exit('Aplikacja chwilowo niedostępna');	
 							}
+						?>
+						</tbody>
+					</table>
+				</div>
+			</div>
+			<div class="col-lg-6 col-md-6">
+				<div class="table-responsive">          
+					<table class="table table-bordered table-hover">
+						<thead>
+							<tr class="danger">
+								<th colspan="6"><p class="table-heading">Wydatki - zestawienie szczegółowe</p></th>
+							</tr>
+								<tr class="danger">
+								 <th class="text-center">#</th>
+								 <th class="text-center">Data</th>
+								 <th class="text-center">Kwota</th>
+								 <th class="text-center">Sposób płatności</th>
+								 <th class="text-center">Kategoria</th>
+								 <th class="text-center">Komentarz</th>
+								</tr>
+						</thead>
+						<tbody>
+						<?php 
+							try{
+								if(!$manager->dbo) {
+									return SERVER_ERROR;
+								}
+								else {
+									$manager->dbo->query("SET CHARSET utf8");
+									$manager->dbo->query("SET NAMES `utf8` COLLATE `utf8_polish_ci`"); 
+								
+									$user_id = $_SESSION['user_id'];
+
+									$query = "SELECT * FROM Expenses e INNER JOIN expenses_category cat INNER JOIN payment_methods pay 				  ON e.category_id = cat.id AND e.user_id = cat.user_id AND e.user_id = pay.user_id 					  AND e.payment_id = pay.id WHERE e.user_id = '$user_id' ORDER BY amount DESC";
+													
+									if(!$result = $manager->dbo->query($query)){
+										//echo 'Wystąpił błąd: nieprawidłowe zapytanie...';
+										return SERVER_ERROR;
+									}
+													
+									if($result->num_rows > 0){
+										$expense_number = 1;
+										$expenses_in_period = 0;
+																									
+									while ($row = $result->fetch_row()){
+										$current_exp_date = $row[5];
+																											
+										if(($current_exp_date>=$min_date)&&($current_exp_date<=$max_date)){	
+																														
+										echo '<tr class="danger">';
+										echo "<td>".$expense_number."</td>";
+										echo "<td>".$row[5]."</td>";
+										echo "<td>".$row[4]." PLN"."</td>";
+										echo "<td>".ucfirst($row[14])."</td>";
+										echo "<td>".ucfirst($row[10])."</td>";
+										echo "<td>".ucfirst($row[6])."</td>";
+										echo "</tr>";
+											$expense_number++;
+											$expenses_in_period++;
+										}
+									} 
+										if($expenses_in_period==0) {
+											echo '<tr class="danger">';
+											echo '<td colspan="6">W wybranym okresie Użytkownik nie ma żadnych wydatków!</td>';
+											echo "</tr>";
+										}
+									} else {
+										echo '<tr class="danger">';
+										echo '<td colspan="6">W wybranym okresie Użytkownik nie ma żadnych wydatków!</td>';
+										echo "</tr>";
+									}
+								}
+						} catch (Exception $e){
+							//echo 'Błąd: ' . $e->getMessage();
+							exit('Aplikacja chwilowo niedostępna');	
+						}
 						?>
 						</tbody>
 					</table>
--- templates/temp.php
+++ templates/temp.php
@@ -1,61 +1,59 @@
 <div class="col-lg-6 col-md-6">
-				<div class="table-responsive">          
-					<table class="table table-bordered table-hover">
-						<thead>
-							<tr class="danger">
-								<th colspan="6"><p class="table-heading">Wydatki - zestawienie szczegółowe</p></th>
-							</tr>
-							<tr class="danger">
-							 <th class="text-center">#</th>
-							 <th class="text-center">Data</th>
-							 <th class="text-center">Kwota</th>
-							 <th class="text-center">Sposób płatności</th>
-							 <th class="text-center">Kategoria</th>
-							 <th class="text-center">Komentarz</th>
-							</tr>
-						</thead>
-						<tbody>
-							<?php 
-							require_once "connect.php";
-								
-								mysqli_report(MYSQLI_REPORT_STRICT);
-								
-								try{
-									$connection = new mysqli($host, $db_user, $db_password, $db_name);
-									if($connection->connect_errno!=0){
-										throw new Exception(mysqli_connect_errno());
-									} else {
-										$connection->query("SET CHARSET utf8");
-										$connection->query("SET NAMES `utf8` COLLATE `utf8_polish_ci`"); 
-										$user_id = $_SESSION['user_id'];
+	<div class="table-responsive">          
+		<table class="table table-bordered table-hover">
+			<thead>
+				<tr class="danger">
+					<th colspan="6"><p class="table-heading">Wydatki - zestawienie szczegółowe</p></th>
+				</tr>
+					<tr class="danger">
+					 <th class="text-center">#</th>
+					 <th class="text-center">Data</th>
+					 <th class="text-center">Kwota</th>
+					 <th class="text-center">Sposób płatności</th>
+					 <th class="text-center">Kategoria</th>
+					 <th class="text-center">Komentarz</th>
+					</tr>
+			</thead>
+			<tbody>
+				<?php 
+					try{
+						if(!$manager->dbo) {
+							return SERVER_ERROR;
+								}
+								else {
+									$manager->dbo->query("SET CHARSET utf8");
+									$manager->dbo->query("SET NAMES `utf8` COLLATE `utf8_polish_ci`"); 
+
+									$user_id = $_SESSION['user_id'];
+
+									$query = "SELECT * FROM Expenses e INNER JOIN expenses_category cat INNER JOIN payment_methods pay ON e.category_id = cat.id AND e.user_id = cat.user_id AND e.user_id = pay.user_id AND e.payment_id = pay.id WHERE e.user_id = '$user_id' ORDER BY amount DESC";
 										
-										$result = $connection->query("SELECT * FROM expenses e INNER JOIN expenses_category cat INNER JOIN payment_methods pay ON e.category_id = cat.id AND e.user_id = cat.user_id AND e.user_id = pay.user_id AND e.payment_id = pay.id WHERE e.user_id = '$user_id' ORDER BY amount DESC");
+									if(!$result = $manager->dbo->query($query)){
+									    //echo 'Wystąpił błąd: nieprawidłowe zapytanie...';
+									    return SERVER_ERROR;
+										}
 										
-										if(!$result) throw new Exception($connection->error);
-										
-										$matched_expenses = $result->num_rows;
-										if($matched_expenses>0) {
-											
-											$expense_number = 1;
-											$expenses_in_period = 0;
+									if($result->num_rows > 0){
+										$expense_number = 1;
+										$expenses_in_period = 0;
 																						
-											while ($row = $result->fetch_assoc()){
-												$current_exp_date = $row['date'];
+										while ($row = $result->fetch_row()){
+											$current_exp_date = $row[5];
 																								
-												if(($current_exp_date>=$min_date)&&($current_exp_date<=$max_date)){	
+											if(($current_exp_date>=$min_date)&&($current_exp_date<=$max_date)){	
 																										
-													echo '<tr class="danger">';
-													echo "<td>".$expense_number."</td>";
-													echo "<td>".$row['date']."</td>";
-													echo "<td>".$row['amount']." PLN"."</td>";
-													echo "<td>".ucfirst($row['payname'])."</td>";
-													echo "<td>".ucfirst($row['name'])."</td>";
-													echo "<td>".ucfirst($row['comment'])."</td>";
-													echo "</tr>";
-													$expense_number++;
-													$expenses_in_period++;
-												}
-											} 
+												echo '<tr class="danger">';
+												echo "<td>".$expense_number."</td>";
+												echo "<td>".$row[5]."</td>";
+												echo "<td>".$row[4]." PLN"."</td>";
+												echo "<td>".ucfirst($row[14])."</td>";
+												echo "<td>".ucfirst($row[10])."</td>";
+												echo "<td>".ucfirst($row[6])."</td>";
+												echo "</tr>";
+												$expense_number++;
+												$expenses_in_period++;
+											}
+										} 
 											if($expenses_in_period==0) {
 												echo '<tr class="danger">';
 												echo '<td colspan="6">W wybranym okresie Użytkownik nie ma żadnych wydatków!</td>';
@@ -66,13 +64,13 @@
 											echo '<td colspan="6">W wybranym okresie Użytkownik nie ma żadnych wydatków!</td>';
 											echo "</tr>";
 										}
-										$connection->close();
+										
 									}
 									
-								} catch(Exception $e){
-									echo '<span style="color:red;">Błąd serwera! Przepraszamy za niedogodności i prosimy o rejestrację w innym terminie! </span>';
-									echo '<br/> Informacja developerska: '.$e;
-								}
+								} catch (Exception $e){
+								//echo 'Błąd: ' . $e->getMessage();
+								exit('Aplikacja chwilowo niedostępna');	
+							}
 						?>
 						</tbody>
 					</table>
